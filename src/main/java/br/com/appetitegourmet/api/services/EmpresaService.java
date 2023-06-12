cdfef3ff58fb7cad1e7fe3e97a3926fe9108779a
Ricardo Oliveira Souza <ricardooliveirasouza@gmail.com>
2023-06-12 03:05:42 -0300

    cruds parcial

--- src/main/java/br/com/appetitegourmet/api/services/EmpresaService.java
+++ src/main/java/br/com/appetitegourmet/api/services/EmpresaService.java
@@ -7,14 +7,20 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import br.com.appetitegourmet.api.models.Empresa;
+import br.com.appetitegourmet.api.models.Endereco;
 import br.com.appetitegourmet.api.repositories.EmpresaRepository;
+import br.com.appetitegourmet.api.repositories.EnderecoRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class EmpresaService {
     private final EmpresaRepository empresaRepository;
+    private final EnderecoRepository enderecoRepository;
 
-    public EmpresaService(EmpresaRepository empresaRepository) {
+    public EmpresaService(EmpresaRepository empresaRepository, 
+    					  EnderecoRepository enderecoRepository) {
         this.empresaRepository = empresaRepository;
+		this.enderecoRepository = enderecoRepository;
     }
 
     public List<Empresa> listarEmpresas() {
@@ -27,11 +33,24 @@ public class EmpresaService {
     }
 
     public Empresa salvarEmpresa(Empresa empresa) {
+    	if(empresa.getEndereco() != null) {
+    		Endereco retorno;
+    		retorno = enderecoRepository.save(empresa.getEndereco());
+    		if(retorno == null) {
+    			new Exception("Falha ao inserir o endereço");
+    		}
+    		empresa.setEndereco(retorno);
+    	}
         return empresaRepository.save(empresa);
     }
-
+    @Transactional
     public void excluirEmpresa(Long id) {
+    	Empresa retorno = empresaRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Empresa não encontrada"));
         empresaRepository.deleteById(id);
+        if(retorno.getEndereco() != null) {
+    		enderecoRepository.deleteById(retorno.getEndereco().getId());
+    	}
     }
 
     public Empresa editarEmpresa(Long id, Empresa empresa) {
