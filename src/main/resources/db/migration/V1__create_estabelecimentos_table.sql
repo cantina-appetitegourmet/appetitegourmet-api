f910c8a3061ef44e13171ea979f5c3cdf35e0c59
Thiago Prazeres <thiagoprazeres@gmail.com>
2023-06-18 04:54:14 -0300

    Modelagem do Banco de Dados: Estabelecimento, Pessoas e Contratos

---
+++ src/main/resources/db/migration/V1__create_estabelecimentos_table.sql
@@ -0,0 +1,173 @@
+CREATE TABLE enderecos (
+    id SERIAL PRIMARY KEY,
+    cep VARCHAR(9) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    numero VARCHAR(10),
+    complemento VARCHAR(100)
+);
+INSERT INTO enderecos (cep, logradouro, bairro, cidade, uf, numero, complemento) VALUES ('51150-430', 'Rua José da Silva Lucena', 'Boa Viagem', 'Recife', 'PE', '20', NULL);
+
+CREATE TABLE colegios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) UNIQUE NOT NULL,
+    endereco_id INTEGER REFERENCES enderecos(id),
+    ativo BOOLEAN DEFAULT true
+);
+INSERT INTO colegios (nome) VALUES ('Colégio GGE');
+INSERT INTO colegios (nome) VALUES ('Colégio CBV');
+INSERT INTO colegios (nome) VALUES ('Colégio Equipe');
+INSERT INTO colegios (nome) VALUES ('Colégio Diocesano');
+
+CREATE TABLE cantinas_empresa (
+  id SERIAL PRIMARY KEY,
+  cnpj VARCHAR(18) NOT NULL UNIQUE,
+  inscricaoEstadual VARCHAR(45) UNIQUE,
+  inscricaoMunicipal VARCHAR(45) UNIQUE,
+  razaoSocial VARCHAR(60) NOT NULL UNIQUE,
+  nomeFantasia VARCHAR(60) NOT NULL,
+  endereco_id INTEGER REFERENCES enderecos(id),
+  ativo BOOLEAN DEFAULT true
+);
+INSERT INTO cantinas_empresa (cnpj, inscricaoEstadual, inscricaoMunicipal, razaoSocial, nomeFantasia, endereco_id) VALUES ('24.687.382/0001-31', '0670910-91', NULL, 'Mgd Servicos de Alimentacao LTDA', 'Appetite Gourmet', 1);
+
+CREATE TABLE unidades (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(60) NOT NULL,
+  colegio_id INTEGER REFERENCES colegios(id),
+  cantina_empresa_id INTEGER REFERENCES cantinas_empresa(id),
+  endereco_id INTEGER REFERENCES enderecos(id),
+  ativo BOOLEAN DEFAULT true,
+  UNIQUE (nome, colegio_id)
+);
+-- Inserindo as unidades do Colégio GGE
+INSERT INTO unidades (nome, colegio_id, cantina_empresa_id, endereco_id) VALUES ('Boa Viagem', 1, 1, 1);
+INSERT INTO unidades (nome, colegio_id) VALUES ('Parnamirim', 1);
+INSERT INTO unidades (nome, colegio_id) VALUES ('Benfica', 1);
+INSERT INTO unidades (nome, colegio_id) VALUES ('Caruaru', 1);
+-- Inserindo as unidades do Colégio CBV
+INSERT INTO unidades (nome, colegio_id) VALUES ('Boa Viagem', 2);
+INSERT INTO unidades (nome, colegio_id) VALUES ('Jaqueira', 2);
+-- Inserindo as unidades do Colégio Equipe
+INSERT INTO unidades (nome, colegio_id) VALUES ('Recife', 3);
+-- Inserindo as unidades do Colégio Diocesano
+INSERT INTO unidades (nome, colegio_id) VALUES ('Caruaru', 4);
+
+
+CREATE TABLE series (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(60) UNIQUE NOT NULL,
+    ativo BOOLEAN DEFAULT true
+);
+INSERT INTO series (nome) VALUES ('Educação Infantil');
+INSERT INTO series (nome) VALUES ('Ensino Fundamental I');
+INSERT INTO series (nome, ativo) VALUES ('Ensino Fundamental II', false);
+INSERT INTO series (nome, ativo) VALUES ('Ensino Médio', false);
+
+CREATE TABLE anos_serie (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    serie_id INTEGER REFERENCES series(id),
+    UNIQUE (nome, serie_id)
+);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('Infantil Berçário', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('G1', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('G2', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('G3', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('G4', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('G5', 1);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('1° Ano', 2);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('2° Ano', 2);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('3° Ano', 2);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('4° Ano', 2);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('5° Ano', 2);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('6° Ano', 3);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('7° Ano', 3);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('8° Ano', 3);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('9° Ano', 3);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('1° Ano', 4);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('2° Ano', 4);
+INSERT INTO anos_serie (nome, serie_id) VALUES ('3° Ano', 4);
+
+CREATE TABLE anos_serie_unidade (
+    id SERIAL PRIMARY KEY,
+    ano_serie_id INTEGER REFERENCES anos_serie(id),
+    unidade_id INTEGER REFERENCES unidades(id),
+    UNIQUE (ano_serie_id, unidade_id)
+);
+INSERT INTO anos_serie_unidade (ano_serie_id, unidade_id)
+SELECT anos_serie.id, unidades.id
+FROM anos_serie, unidades;
+
+CREATE TYPE turnos_enum AS ENUM (
+  -- 'M' Manhã
+  'M',
+  -- 'T' Tarde
+  'T',
+  -- 'N' Noite
+  'N'
+);
+CREATE TABLE ano_serie_unidade_turnos (
+  id SERIAL PRIMARY KEY,
+  ano_serie_unidade_id INTEGER REFERENCES anos_serie_unidade(id),
+  turno turnos_enum,
+  UNIQUE (ano_serie_unidade_id, turno)
+);
+INSERT INTO ano_serie_unidade_turnos (ano_serie_unidade_id, turno)
+SELECT a.id, t
+FROM anos_serie_unidade a
+CROSS JOIN unnest(enum_range(null::turnos_enum)) t;
+
+
+CREATE TYPE turmas_enum AS ENUM ('A', 'B', 'C', 'D', 'E', 'F');
+CREATE TABLE turmas (
+  id SERIAL PRIMARY KEY,
+  ano_serie_unidade_turno_id INTEGER REFERENCES ano_serie_unidade_turnos(id),
+  turma turmas_enum,
+  UNIQUE (ano_serie_unidade_turno_id, turma)
+);
+INSERT INTO turmas (ano_serie_unidade_turno_id, turma)
+SELECT ast.id, te
+FROM ano_serie_unidade_turnos ast
+CROSS JOIN unnest(enum_range(NULL::turmas_enum)) AS te;
+
+CREATE TABLE anos_letivos (
+  id SERIAL PRIMARY KEY,
+  ano VARCHAR(6) NOT NULL UNIQUE,
+  ativo BOOLEAN DEFAULT true
+);
+INSERT INTO anos_letivos (ano) VALUES ('2023');
+INSERT INTO anos_letivos (ano) VALUES ('2024');
+
+CREATE TABLE turma_anos_letivos (
+  id SERIAL PRIMARY KEY,
+  turma_id INTEGER REFERENCES turmas(id),
+  ano_letivo_id INTEGER REFERENCES anos_letivos(id),
+  ativo BOOLEAN DEFAULT true,
+  UNIQUE (turma_id, ano_letivo_id)
+);
+INSERT INTO turma_anos_letivos (turma_id, ano_letivo_id)
+SELECT t.id, a.id
+FROM turmas t, anos_letivos a;
+
+CREATE TABLE planos_alimentares (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL UNIQUE,
+  ativo BOOLEAN DEFAULT true
+);
+INSERT INTO planos_alimentares (nome) VALUES ('Lanche regular');
+INSERT INTO planos_alimentares (nome) VALUES ('Lanche complementar');
+INSERT INTO planos_alimentares (nome) VALUES ('Almoço');
+
+CREATE TABLE planos_alimentares_precos (
+  id SERIAL PRIMARY KEY,
+  plano_alimentar_id INTEGER REFERENCES planos_alimentares(id),
+  turma_anos_letivos_id INTEGER REFERENCES turma_anos_letivos(id),
+  preco_dia DECIMAL(10, 2) NOT NULL,
+  ativo BOOLEAN DEFAULT true,
+  UNIQUE (plano_alimentar_id, turma_anos_letivos_id)
+);
+INSERT INTO planos_alimentares_precos (plano_alimentar_id, turma_anos_letivos_id, preco_dia) VALUES (1, 1, 55.00);
+INSERT INTO planos_alimentares_precos (plano_alimentar_id, turma_anos_letivos_id, preco_dia) VALUES (1, 2, 55.50);
---
+++ src/main/resources/db/migration/V3__create_contratos_table.sql
@@ -0,0 +1,75 @@
+CREATE TABLE descontos (
+  id SERIAL PRIMARY KEY,
+  motivo VARCHAR(255),
+  valor_percentual DECIMAL(5, 2) NOT NULL CHECK (valor_percentual >= 0 AND valor_percentual <= 100)
+);
+INSERT INTO descontos (motivo, valor_percentual) VALUES
+  ('2 ou mais filhos', 5),
+  ('Desconto concedido em negociação por serem 3 filhos', 7),
+  ('Desconto especial por ter contratado pacote completo', 10),
+  ('funcionário da escola', 5),
+  ('Desconto especial para familiar', 50),
+  ('Desconto concedido em negociação', 7);
+
+ 
+CREATE TABLE contrato (
+  id SERIAL PRIMARY KEY,
+  aluno_responsavel_id INTEGER REFERENCES responsaveis_alunos(id),
+  turma_anos_letivos_id INTEGER REFERENCES turma_anos_letivos(id),
+  desconto_id INTEGER REFERENCES descontos(id),
+  valor_percentual DECIMAL(4, 2) CHECK (valor_percentual >= 0 AND valor_percentual <= 100),
+  data_adesao DATE DEFAULT CURRENT_DATE,
+  ativo BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- Criação da trigger para preencher turma_anos_letivos_id
+CREATE OR REPLACE FUNCTION set_turma_anos_letivos_id()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  NEW.turma_anos_letivos_id := (
+    SELECT turma_anos_letivos_id
+    FROM responsaveis_alunos
+    WHERE id = NEW.aluno_responsavel_id
+  );
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Criação da trigger que chama a função set_turma_anos_letivos_id
+CREATE TRIGGER fill_turma_anos_letivos_id
+BEFORE INSERT ON contrato
+FOR EACH ROW
+EXECUTE FUNCTION set_turma_anos_letivos_id();
+
+
+CREATE TABLE contrato_planos (
+  id SERIAL PRIMARY KEY,
+  contrato_id INTEGER REFERENCES contrato(id),
+  planos_alimentares_precos_id INTEGER REFERENCES planos_alimentares_precos(id),
+  segunda BOOLEAN,
+  terca BOOLEAN,
+  quarta BOOLEAN,
+  quinta BOOLEAN,
+  sexta BOOLEAN,
+  sabado BOOLEAN,
+  domingo BOOLEAN,
+  preco_dia DECIMAL(10, 2)
+);
+
+CREATE OR REPLACE FUNCTION calcular_preco_dia()
+  RETURNS TRIGGER AS $$
+BEGIN
+  SELECT preco_dia INTO NEW.preco_dia
+  FROM planos_alimentares_precos
+  WHERE id = NEW.planos_alimentares_precos_id;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_calcular_preco_dia
+BEFORE INSERT ON contrato_planos
+FOR EACH ROW
+EXECUTE FUNCTION calcular_preco_dia();
