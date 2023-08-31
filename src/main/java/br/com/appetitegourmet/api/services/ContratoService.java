4df7d913c37f1bcedaba651185c028439122a59a
Ricardo Oliveira Souza <ricardooliveirasouza@gmail.com>
2023-08-31 02:25:51 -0300

    parte 1 do desconto automatico

--- src/main/java/br/com/appetitegourmet/api/services/ContratoService.java
+++ src/main/java/br/com/appetitegourmet/api/services/ContratoService.java
@@ -37,6 +37,9 @@ public class ContratoService {
     }
 
     public Contrato salvarContrato(Contrato contrato) {
+    	Contrato retContrato;
+    	List<Contrato> contratos;
+    	
     	if(contrato.getResponsavelAluno() != null) {
     		ResponsavelAluno retorno;
     		Optional<ResponsavelAluno> opt;
@@ -57,7 +60,35 @@ public class ContratoService {
     		retorno = opt.get();
     		contrato.setTurmaAnoLetivo(retorno);
     	}
-        return contratoRepository.save(contrato);
+    	retContrato = contratoRepository.save(contrato);
+    	contratos = contratoRepository.findAllByResponsavelId(retContrato.getResponsavelAluno().getResponsavel().getId());
+    	if(contratos != null && contratos.size() > 1) {
+    		for(Contrato contratoBD : contratos) {
+	    		Boolean encontrou = false;
+	    		List<ContratoDesconto> lista = contratoDescontoRepository.findByContratoId(contratoBD.getId());
+	    		if(lista.size() > 0) {
+	    			for(ContratoDesconto cd: lista) {
+	    				if(cd.getDesconto().getTipo() == ValidacaoConstantes.DESCONTO_2_OU_MAIS_FILHOS) {
+	    					encontrou = true;
+	    					break;
+	    				}
+	    			}
+	    		}
+	    		if(!encontrou) {
+	    			ContratoDesconto cd = new ContratoDesconto();
+	    			cd.setContrato(contratoBD);
+	    			cd.setDataInicio(contratoBD.getDataAdesao());
+	    			List<Desconto> descontos = descontoRepository.findAll();
+	    			for(Desconto des : descontos) {
+	    				if(des.getTipo() == ValidacaoConstantes.DESCONTO_2_OU_MAIS_FILHOS) {
+	    					cd.setDesconto(des);
+	    				}
+	    			}
+	    			contratoDescontoRepository.save(cd);
+	    		}
+    		}
+    	}
+        return retContrato;
     }
 
     public void excluirContrato(Long id) {
