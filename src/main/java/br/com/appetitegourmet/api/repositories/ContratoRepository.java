d22b7cb435a66e0854f2bc5c5f4a71a03634a763
Ricardo Oliveira Souza <ricardooliveirasouza@gmail.com>
2023-08-31 02:33:13 -0300

    parte 2 do desconto automatico

--- src/main/java/br/com/appetitegourmet/api/repositories/ContratoRepository.java
+++ src/main/java/br/com/appetitegourmet/api/repositories/ContratoRepository.java
@@ -3,6 +3,7 @@ package br.com.appetitegourmet.api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.appetitegourmet.api.models.Contrato;
@@ -14,4 +15,12 @@ public interface ContratoRepository extends JpaRepository<Contrato, Long> {
 
 	List<ResponsavelAluno> findByResponsavelAlunoId(Long responsavelAlunoId);
 	List<TurmaAnoLetivo> findByTurmaAnoLetivoId(Long turmaAnoLetivoId);
+	
+	@Query("select con from Responsavel res, "
+			             + "ResponsavelAluno ra, "
+			             + "Contrato con "
+		 + "where res.id = ra.responsavel_id and "
+		    + "ra.id = con.responsavel_aluno.id and "
+		    + "res.id = ?1")
+	List<Contrato> findAllByResponsavelId(Long responsavelId);
 }
--- src/main/java/br/com/appetitegourmet/api/services/ContratoService.java
+++ src/main/java/br/com/appetitegourmet/api/services/ContratoService.java
@@ -7,11 +7,16 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import br.com.appetitegourmet.api.models.Contrato;
+import br.com.appetitegourmet.api.models.ContratoDesconto;
+import br.com.appetitegourmet.api.models.Desconto;
 import br.com.appetitegourmet.api.models.ResponsavelAluno;
 import br.com.appetitegourmet.api.models.TurmaAnoLetivo;
+import br.com.appetitegourmet.api.repositories.ContratoDescontoRepository;
 import br.com.appetitegourmet.api.repositories.ContratoRepository;
+import br.com.appetitegourmet.api.repositories.DescontoRepository;
 import br.com.appetitegourmet.api.repositories.ResponsavelAlunoRepository;
 import br.com.appetitegourmet.api.repositories.TurmaAnoLetivoRepository;
+import utils.ValidacaoConstantes;
 
 @Service
 public class ContratoService {
@@ -19,11 +24,15 @@ public class ContratoService {
 	private final ContratoRepository contratoRepository;
 	private final ResponsavelAlunoRepository responsavelAlunoRepository;
 	private final TurmaAnoLetivoRepository turmaAnoLetivoRepository;
+	private final ContratoDescontoRepository contratoDescontoRepository;
+	private final DescontoRepository descontoRepository;
 	
-	public ContratoService(TurmaAnoLetivoRepository turmaAnoLetivoRepository, ResponsavelAlunoRepository responsavelAlunoRepository, ContratoRepository contratoRepository) {
+	public ContratoService(TurmaAnoLetivoRepository turmaAnoLetivoRepository, ResponsavelAlunoRepository responsavelAlunoRepository, ContratoRepository contratoRepository, ContratoDescontoRepository contratoDescontoRepository, DescontoRepository descontoRepository) {
 		this.contratoRepository = contratoRepository;
 		this.responsavelAlunoRepository = responsavelAlunoRepository;
 		this.turmaAnoLetivoRepository = turmaAnoLetivoRepository;
+		this.contratoDescontoRepository = contratoDescontoRepository;
+		this.descontoRepository = descontoRepository;
 		
 	}
 	
